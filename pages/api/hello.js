0e28e8c0d368705036e2956efd3f397aba2cd210
Guus <guus.noij@gmail.com>
2022-12-05 20:42:57 +0100

    new fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,12 +2,4 @@
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
-
-  fetch('/', {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(req.body).toString(),
-  })
-
-  return true
 }
